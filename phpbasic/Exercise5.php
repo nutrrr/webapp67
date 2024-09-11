8dc0066666fa595fcc774c8eed08cf73e56bf5c1
nutrrr <100115258+nutrrr@users.noreply.github.com>
2024-09-11 11:18:57 +0700

    add test case

--- phpbasic/Exercise5.php
+++ phpbasic/Exercise5.php
@@ -11,6 +11,8 @@
         function test($n){
             return $n % 3 == 0 || $n % 7 == 0;
         }
+        var_dump(test(3));
+        echo "<br>";
         var_dump(test(14));
         echo "<br>";
         var_dump(test(12));
